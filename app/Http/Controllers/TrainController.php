acc3c460abde51ce9f27bfe2ad4f95c33d48aaf7
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-09-26 18:10:46 +0200

    [UPD] fix codice

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Trains; // Assicurati che sia Train, non Trains
+use App\Models\Trains;
 use Carbon\Carbon;
 
 class TrainController extends Controller
 {
     public function index()
     {
-        // Recupera i treni in partenza dalla data odierna
+
         $trains = Trains::where('departure_time', '>=', Carbon::today())->get();
 
         return view('welcome', compact('trains'));
