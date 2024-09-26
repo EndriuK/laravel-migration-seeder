8f79527d2daef7d0158561e42a067e7dfcd8bad3
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-09-26 16:52:31 +0200

    [UPD] create voci schema tabella

--- database/migrations/2024_09_26_144444_create_trains_table.php
+++ database/migrations/2024_09_26_144444_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 50);
+            $table->string('carriages', 50);
+            $table->string('on_time', 50);
+            $table->string('cancelled', 50);
             $table->timestamps();
         });
     }
